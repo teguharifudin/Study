d9a7cbe04d76f562dd870a4f32895b470e545817
Teguh Arief <114749743+teguharifudin@users.noreply.github.com>
2024-09-14 12:21:24 +0700

    Update StockRepository.java

--- src/main/java/com/example/study/repository/StockRepository.java
+++ src/main/java/com/example/study/repository/StockRepository.java
@@ -1,18 +1,17 @@
 package com.example.study.repository;
 
 import com.example.study.model.Stock;
-// import com.example.study.dto.StockDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-// import java.util.List;
+import java.util.List;
 import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, Integer> {
 
-    // @Query(value = "SELECT * FROM stock", nativeQuery = true)
-    // List<Stock> getStocks();
+    @Query(value = "SELECT * FROM stock", nativeQuery = true)
+    List<Stock> getStocks();
 
     @Query(value = "SELECT * FROM stock WHERE id = :id", nativeQuery = true)
     Optional<Stock> findById(Integer id);
