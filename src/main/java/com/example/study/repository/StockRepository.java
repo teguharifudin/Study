78bab8acc3adbcd82f0c941043760a1698b91b8b
Teguh Arief <114749743+teguharifudin@users.noreply.github.com>
2024-09-15 11:24:58 +0700

    Update StockRepository.java

--- src/main/java/com/example/study/repository/StockRepository.java
+++ src/main/java/com/example/study/repository/StockRepository.java
@@ -1,5 +1,6 @@
 package com.example.study.repository;
 
+import com.example.study.dto.StockDto;
 import com.example.study.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -15,6 +16,9 @@ public interface StockRepository extends JpaRepository<Stock, Integer> {
 
     @Query(value = "SELECT * FROM stock WHERE id = :id", nativeQuery = true)
     Optional<Stock> findById(Integer id);
+
+    @Query(value = "INSERT INTO stock (nama, stok, nomor, gambar, attributes, usr, created, rev, updated) VALUES (nama, stok, nomor, file, attributes, usr, created, rev, updated)", nativeQuery = true)
+    Stock save(StockDto stockDto);
 
     @Modifying
     @Query(value = "DELETE FROM stock WHERE id = :id", nativeQuery = true)
